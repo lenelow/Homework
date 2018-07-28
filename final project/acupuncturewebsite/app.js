46b024b79142483bf4bf089481645d4432d92378
Leah Enelow <lenelow@students.terpmail.edu>
2018-07-28 17:03:15 -0400

    temporary website

--- final project/acupuncturewebsite/app.js
+++ final project/acupuncturewebsite/app.js
@@ -2,4 +2,9 @@ jQuery(document).ready(function() {
   jQuery("header nav ul li:first-child").click(function(){
     jQuery(this).parent().toggleClass("slideDown");
   });
+  jQuery("header nav ul li a").hover(function(){
+    jQuery(this).css("color", "white");
+    }, function(){
+    jQuery(this).css("color", "black");
+  });
 });
